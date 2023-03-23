618da99e2329b2e2522c9e215cda9b3763f4c05a
Lucretius Biah <lucretiusbiah@protonmail.com>
2023-03-23 23:19:14 +0000

    Trying to index articles

--- src/routes/blog/index.ts
+++ src/routes/blog/index.ts
@@ -1,3 +1,15 @@
-export getArticlesList: Function = ()=> {
+export const getArticlesList = async () : Promise<BlogPost[]> => {
+    const _import = import.meta.glob("./posts/**/*.md", {
+        import: "metadata"
+    });
+    const files: BlogPost[] = [];
 
+    for (const path in _import){
+        let slug = path.split("/")[2];
+        let index: any = await _import[path]();
+        const metadata: BlogPost = {...index!, slug }
+        files.push(metadata);
+    }
+
+    return files;
 }
