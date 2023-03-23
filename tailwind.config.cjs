27c6873e435e985d9e012932cd39a4c15e6389c6
Lucretius Biah <lucretiusbiah@protonmail.com>
2023-03-23 23:17:37 +0000

    Added custom screens to tailwindcss

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,14 @@ const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
+		screens: {
+			"xs": "360px",
+			"mobile": "640px",
+			"tablet": "768px",
+			"laptop": "1024px",
+			"desktop": "1280px",
+			"tv": "1536px"
+		},
 		extend: {}
 	},
 
