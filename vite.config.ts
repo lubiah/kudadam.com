e0e965d39770dde0c59127814c93ed6101076bff
Lucretius Biah <lucretiusbiah@protonmail.com>
2023-03-25 19:16:53 +0000

    Added a library to allow me import svg

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import svg from "@poppanator/sveltekit-svg";
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		svg()
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
