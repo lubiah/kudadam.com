22fe75824c2d795d3cee195f1bb27bda09cc06a6
Lucretius Biah <lucretiusbiah@protonmail.com>
2023-03-26 17:35:50 +0000

    Working on blog slug page

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -11,11 +11,11 @@
 
 <main class="max-w-screen-laptop mx-auto min-w-0">
     <article>
-        <h1 class="mb-12 text-gray-800">{data.metadata.title}</h1>
-        <img src={data.metadata.image} alt="" class="w-full h-auto max-h-[250px] mb-6 !col-[1/-1]">
-        <ul class="text-gray-500 mb-6 text-sm flex flex-col gap-2">
-        <li class="flex items-center gap-x-1"><ClockIcon height="20" width="20"/><span>{data.metadata.readingTime.text}</span></li>
-        <li class="flex items-center gap-x-1"><CalendarIcon height="20" width="20"/><time datetime={new Date(data.metadata.date).toISOString()}>{new Date(data.metadata.date).toLocaleString('en-US', { dateStyle: 'long' })}</time></li>
+        <h1 class="mb-12 tablet:mb-16 text-gray-800">{data.metadata.title}</h1>
+        <img src={data.metadata.image} alt="" class="w-full h-auto max-h-[250px] mb-6 tablet:mb-8 !col-[1/-1]">
+        <ul class="text-gray-500 mb-6 tablet:mb-8 text-xs flex flex-col tablet:flex-row gap-2 tablet:gap-x-6">
+        <li class="flex items-center gap-x-1"><ClockIcon height="16" width="16"/><span>{data.metadata.readingTime.text}</span></li>
+        <li class="flex items-center gap-x-1"><CalendarIcon height="16" width="16"/><time datetime={new Date(data.metadata.date).toISOString()}>{new Date(data.metadata.date).toLocaleString('en-US', { dateStyle: 'long' })}</time></li>
         </ul>
         <svelte:component this={data.component}/>
     </article>
