77031ab5e013842dd98cd14bdafdd6db9202f4fc
Lucretius Biah <lucretiusbiah@protonmail.com>
2023-03-27 12:29:19 +0000

    Fixed the page for tablet versions

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -9,14 +9,14 @@
 
 <main class="max-w-screen-laptop mx-auto min-w-0">
 	<article>
-		<h1 class="mb-12 tablet:mb-16 text-gray-800">{data.metadata.title}</h1>
+		<h1 class="mb-12 tablet:mb-16 laptop:mb-20 laptop:!col-[1/-1]">{data.metadata.title}</h1>
 		<img
 			src={data.metadata.image}
 			alt=""
-			class="w-full h-auto max-h-[250px] mb-6 tablet:mb-8 !col-[1/-1]"
+			class="w-full h-auto max-h-[250px] tablet:max-h-[400px] laptop:max-h-[600px] mb-6 tablet:mb-8 laptop:mb-10 !col-[1/-1]"
 		/>
 		<ul
-			class="text-gray-500 mb-6 tablet:mb-8 text-xs flex flex-col tablet:flex-row gap-2 tablet:gap-x-6"
+			class="text-gray-500 mb-6 tablet:mb-8 laptop:mb-10 text-xs laptop:text-base flex flex-col tablet:flex-row gap-2 tablet:gap-x-6 !col-[1/-1]"
 		>
 			<li class="flex items-center gap-x-1">
 				<ClockIcon height="16" width="16" /><span>{data.metadata.readingTime}</span>
@@ -32,7 +32,7 @@
 	</article>
 </main>
 
-<style type="text/postcss">
+<style lang="postcss">
 	article {
 		@apply grid grid-cols-[1fr_min(65ch,calc(100%-32px))_1fr];
 	}
