85773d2c292be120aab996f28ddbae87390fa576
Lucretius Biah <lucretiusbiah@protonmail.com>
2023-04-10 03:12:07 +0000

    Draft for new article

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -23,7 +23,7 @@
 	<img
 		src={data.metadata.image}
 		alt=""
-		class="w-full h-auto max-h-[250px] tablet:max-h-[400px] mb-6 tablet:mb-8 laptop:mb-10 !col-[1/-1]"
+		class="w-full h-auto max-h-[250px] tablet:max-h-[400px] laptop:max-h-[550px] mb-6 tablet:mb-8 laptop:mb-10 !col-[1/-1]"
 	/>
 	<ul
 		class="text-gray-500 mb-6 tablet:mb-8 laptop:mb-10 text-xs laptop:text-base flex flex-col tablet:flex-row gap-2 tablet:gap-x-6 mx-4 laptop:mx-0 p-0"
